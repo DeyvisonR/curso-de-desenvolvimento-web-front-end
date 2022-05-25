aacc9a37763d23448e8b4a77896cc19bba9e3f89
DeyvisonR <deyvisondosreis17@gmail.com>
2022-05-25 06:03:02 -0300

    att

---
+++ curso de front-end com foco no js/react/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+    <div id="main"></div>
+
+    <script crossorigin src="react.production.min"></script>
+    <script crossorigin src="react-dom.production.min"></script>
+    <script src="babel.min.js"></script>
+
+    <script type="text/babel">
+        ReactDOM.render(<h1>ola mundo do react</h1>,document.getElementById('main'))
+    </script>
+        
+    </body>
+</html>
