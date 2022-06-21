431d9ff5b2fb2fa358c24cebb4aa12e1f73c24d2
DeyvisonR <deyvisondosreis17@gmail.com>
2022-06-20 22:06:03 -0300

    att

---
+++ node_projects/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const bodyparser = require("body-parser");
+const path = require("path");
+const { urlencoded } = require("body-parser");
+app.listen("3000", ()=>{
+
+});
+
+// Body parser
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyparser.json());
+app.use(urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// criando conexão com o banco de dados
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_testes"
+});
+
+db.connect(function(err){
+    if(err){
+        console.log("Erro!!! não foi possivel conectar ao banco de dados");
+    }
+    var sql = "SELECT * FROM `clientes`";
+    db.query(sql, function(err, results){
+        console.log(results);
+    });
+})
+
+// renderizar a pagina
+
+app.get("/", (req, res)=>{
+    // res.render("index", {'nome': 'deyvison', "profissao": 'programador'});
+    // acima está como passar parametros isolados
+});
+
+app.get("/cadastrar", (req, res)=>{
+    // res.render("cadastrar", {lista:[{'telefone': '9 9999-9999'}, {"telefone": "9 8888-8888"}, {"telefone": "9 7777-7777"}]});
+    // acima esta como passar um parametro com varios valores
+    res.render("cadastrar", {});
+});
